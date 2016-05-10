7df8147e78f8fc3e6f4d4ec853f7a93fc6b9fbb6
helondeng <dhlwust@163.com>
2016-05-10 15:13:57 +0800

    support string

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-    
-var fs = require('fs');
+    var fs = require('fs');
 
-function parse(cssFileUri) {
-	var cssContent = fs.readFileSync(cssFileUri, {
-		encoding: 'utf8'
-	});
-	cssContent = cssContent.replace(/[\n|\r]/g, " ");
-	cssContent = cssContent.replace(/'/g, '\"');
-	cssContent = "'" + cssContent + "'";
-	return cssContent;
-}
+    function parse(cssFileUri) {
+    	var cssContent;
+    	if ( /\.css$/.test(cssFileUri)){
+    		cssContent = fs.readFileSync(cssFileUri, {
+    			encoding: 'utf8'
+    		});
+    	} else {
+    		cssContent = cssFileUri;
+    	}
+    	cssContent = cssContent.replace(/[\n|\r]/g, " ");
+    	cssContent = cssContent.replace(/'/g, '\"');
+    	cssContent = "'" + cssContent + "'";
+    	return cssContent;
+    }
 
-module.exports = {
-	parse: parse
-};
+    module.exports = {
+    	parse: parse
+    };
